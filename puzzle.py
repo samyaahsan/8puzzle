766508fb4965c528dbee288849f0fd0a58e254ec
samyaahsan <96796200+samyaahsan@users.noreply.github.com>
2022-10-02 01:12:00 -0400

    bfs and dfs works

--- puzzle.py
+++ puzzle.py
@@ -16,17 +16,18 @@ import queue as Q
 class Frontier(object):
     # each node == puzzle state
     # keep a mirror of the queue as a set for checking
-    #make it O(1)
+    # make it O(1)
 
     def __init__(self, switch):
         if switch == "queue":
             self.q = Q.Queue()
             self.qset = set()
-
-        if switch == "stack":
+        elif switch == "stack":
             self.stack = []
+            self.sset = set()
         pass
 
+    #queue methods
     def put(self, node):
         self.qset.add(tuple(node.config))
         return self.q.put(node)
@@ -44,6 +45,26 @@ class Frontier(object):
             return True
         else:
             return False
+
+    #stack methods
+    def stack_empty(self):
+        return len(self.stack) == 0
+
+    def push(self, node):
+        self.sset.add(tuple(node.config))
+        return self.stack.append(node)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def stack_search(self, node):
+        if tuple(node.config) in self.sset:
+            return True
+        else:
+            return False
+
+
+
 
 
 
@@ -90,14 +111,16 @@ class PuzzleState(object):
         :return a PuzzleState with the new configuration
         """
         #(self, config, n, parent=None, action="Initial", cost=0):
-        new_state = PuzzleState(self.config[:], self.n, self, self.action, self.cost)
+        new_state = PuzzleState(self.config[:], self.n, self, self.action, self.cost + 1)
         if new_state.blank_index > self.n:
             new_state.config[new_state.blank_index], new_state.config[new_state.blank_index - self.n] = new_state.config[
                                                                                                        new_state.blank_index - self.n], \
                                                                                                    new_state.config[
                                                                                                        new_state.blank_index]
+            new_state.action = "Up"
         else:
             return None
+
         return new_state
         pass
 
@@ -112,6 +135,9 @@ class PuzzleState(object):
                                                                                                        new_state.blank_index + self.n], \
                                                                                                    new_state.config[
                                                                                                        new_state.blank_index]
+            new_state.action = "Down"
+        else:
+            return None
 
         return new_state
         pass
@@ -127,6 +153,9 @@ class PuzzleState(object):
                                                                                                        new_state.blank_index - 1], \
                                                                                                    new_state.config[
                                                                                                        new_state.blank_index]
+            new_state.action = "Left"
+        else:
+            return None
         return new_state
         pass
 
@@ -141,11 +170,14 @@ class PuzzleState(object):
         #  6, 7, 8]
 
         new_state = PuzzleState(self.config[:], self.n, self, self.action, self.cost)
-        if (new_state.blank_index % self.n) != 1 and new_state.blank_index < self.n:
+        if (new_state.blank_index % self.n) != self.n - 1:  # and new_state.blank_index < self.n:
             new_state.config[new_state.blank_index], new_state.config[new_state.blank_index + 1] = new_state.config[
                                                                                                        new_state.blank_index + 1], \
                                                                                                    new_state.config[
                                                                                                        new_state.blank_index]
+            new_state.action = "Right"
+        else:
+            return None
         return new_state
         pass
 
@@ -185,10 +217,11 @@ def bfs_search(initial_state):
     # use explicit queue
     frontier = Frontier("queue")
     frontier.put(initial_state)
-    #print(frontier.get().config)
     explored = set()
+    moves = []
 
     while not frontier.empty():
+        # print(explored)
         state = frontier.get()
         #print(state.config)
         explored.add(tuple(state.config))
@@ -196,22 +229,52 @@ def bfs_search(initial_state):
 
         #print(test_goal(state))
         if test_goal(state):
-            print(state.config)
-            return state
+            #print(state.config)
+            while state.parent:
+                moves.append(state.action)
+                state = state.parent
+            print(moves[::-1])
+            return True
 
         for neighbor in state.expand():
             #print(neighbor.config)
-            if not frontier.search(neighbor) and neighbor not in explored:
+            if tuple(neighbor.config) not in explored and not frontier.search(neighbor):
+            #if tuple(neighbor.config) not in explored:
                 frontier.put(neighbor)
 
-    return -1
-    pass
+    return False
+    # pass
 
 
 def dfs_search(initial_state):
     """DFS search"""
     ### STUDENT CODE GOES HERE ###
     # queue holds puzzle states
+    #when expand, reverse the list
+    frontier = Frontier("stack")
+    frontier.push(initial_state)
+    #print(frontier.pop().config)
+    explored = set()
+    moves = []
+
+    while not frontier.stack_empty():
+        state = frontier.pop()
+        #print(state.config)
+        explored.add(tuple(state.config))
+
+        if test_goal(state):
+            while state.parent:
+                #print(state.config)
+                moves.append(state.action)
+                state = state.parent
+            print(moves[::-1])
+            return True
+
+        for neighbor in state.expand()[::-1]:
+            if not frontier.stack_search(neighbor) and neighbor not in explored:
+                frontier.push(neighbor)
+
+    return False
     pass
 
 
@@ -236,9 +299,13 @@ def calculate_manhattan_dist(idx, value, n):
 def test_goal(puzzle_state):
     """test the state is the goal state or not"""
     ### STUDENT CODE GOES HERE ###
-    new_list = sorted(puzzle_state.config)
+    copy = puzzle_state.config.copy()
+   # print(copy)
+
+    new_list = sorted(copy)
     #print(sorted(puzzle_state.config))
     if (puzzle_state.config == new_list):
+        print(puzzle_state.config)
         return True
     else:
         return False
@@ -255,8 +322,9 @@ def main():
     start_time = time.time()
 
     if search_mode == "bfs":
-        bfs_search(hard_state)
-    # elif search_mode == "dfs": dfs_search(hard_state)
+        print(bfs_search(PuzzleState([6,1,8,4,0,2,7,3,5], 3)))
+    elif search_mode == "dfs":
+        print(dfs_search(hard_state))
     # elif search_mode == "ast": A_star_search(hard_state)
     else:
         print("Enter valid command arguments !")
